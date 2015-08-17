e2b8b4ee41b87743365c753dd83d664cba08c691
Carine Vunabandi <carine.vunabandi@bt.com>
2015-08-17 16:44:14 +0100

    Rename step following conventions

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -3,9 +3,9 @@ When "I visit the homepage" do
 end
 
 Then "I should see 'this month's budget' tab" do
-  expect(page.body).to have_content("This month's bugdet")
+  expect(page.body).to have_content("This month's budget")
 end
 
 Then "I should see 'View previous budgets' tab" do
-  expect(page.body).to have_content("View previous bugdets")
+  expect(page.body).to have_content("View previous budgets")
 end
