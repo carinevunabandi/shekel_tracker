ab700670a009b980b1700909ea87b6828f2ba8fb
Carine Vunabandi <carine.vunabandi@bt.com>
2015-08-17 11:35:30 +0100

    Setup RSpec configurations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,15 @@
 ENV['RACK_ENV'] = 'test'
 
 require File.expand_path('../../shekel_tracker.rb', __FILE__)
+
+require 'capybara/rspec'
+require 'rspec'
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+end
+
+def app
+  ShekelTracker
+end
+#Dir[File.join(Sinatra::Application.root, 'spec', 'support', '**', '*.rb')].each { |f| require f }
