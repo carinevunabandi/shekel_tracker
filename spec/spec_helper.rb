fd1c2aaf048480850dbaca2a189a3025865ba16d
Carine Vunabandi <carine.vunabandi@bt.com>
2015-08-17 11:42:10 +0100

    Remove unnecessary code from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ ENV['RACK_ENV'] = 'test'
 require File.expand_path('../../shekel_tracker.rb', __FILE__)
 
 require 'capybara/rspec'
-require 'rspec'
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods
@@ -12,4 +11,3 @@ end
 def app
   ShekelTracker
 end
-#Dir[File.join(Sinatra::Application.root, 'spec', 'support', '**', '*.rb')].each { |f| require f }
