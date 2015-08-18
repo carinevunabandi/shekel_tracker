aa8106f29d6568189c977e814e7be1f1e8c0ec5e
Carine Vunabandi <carine.vunabandi@bt.com>
2015-08-18 09:57:52 +0100

    Add date column to costs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150602143208) do
+ActiveRecord::Schema.define(version: 20150818085103) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name",        limit: 255
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150602143208) do
     t.text     "description",    limit: 65535
     t.string   "category_id",    limit: 255
     t.datetime "time_period_id"
+    t.datetime "date"
   end
 
   create_table "time_periods", force: :cascade do |t|
