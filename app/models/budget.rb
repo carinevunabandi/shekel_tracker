813f69fe4f3336ce5a66fe723c12a7aaab24ee68
Carine Vunabandi <carine.vunabandi@bt.com>
2015-08-18 17:19:52 +0100

    Add budget model and spec

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,3 +1,9 @@
 class Budget < ActiveRecord::Base
   belongs_to :time_period
+
+  def self.amount_for_range(from, to)
+    time_period_id = TimePeriod.where(from:from, to:to).time_period_id
+    budget = Budget.where(time_period_id: time_period_id).first
+    budget.amount
+  end
 end
---
+++ spec/models/budget_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Budget do
+  let(:time_period) { double(:time_period, id: 1, from:'1-May-2015', to:'31-May-2015') }
+
+  describe '.amount_for_range' do
+    it 'tells what the budget for a given time period is' do
+      Budget.create(time_period_id: 1, amount: 500)
+      allow(TimePeriod).to receive_message_chain(:where,:time_period_id).and_return(1)
+      expect(Budget.amount_for_range('1-May-2015','31-May-2015')).to eq 500
+    end
+  end
+end
