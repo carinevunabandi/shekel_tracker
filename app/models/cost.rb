5d05bb8c38d8ca521b3949562a88ceefc65cbffa
Carine Vunabandi <carine.vunabandi@bt.com>
2015-08-19 23:55:09 +0100

    Work out total costs given a period of time

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -1,4 +1,13 @@
 class Cost < ActiveRecord::Base
   belongs_to :category
   belongs_to :time_period
+
+  def self.total_for_period(time_period_id)
+    costs = Cost.where(time_period_id: time_period_id)
+    prices = []
+    if !costs.nil?
+      prices = costs.map(&:price)
+    end
+    prices.reduce(:+)
+  end
 end
--- spec/models/cost_spec.rb
+++ spec/models/cost_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Cost do
+  describe '.total_for_period' do
+    let(:time_period) { double(:time_period, id: 1) }
+    let(:cost_1) { double(:cost_1, price: 10, time_period_id: 1) }
+    let(:cost_2) { double(:cost_2, price: 20, time_period_id: 2) }
+    let(:cost_3) { double(:cost_3, price: 30, time_period_id: 1) }
 
+    it 'loads all expenses that occured during the specified time period' do
+      expect(Cost).to receive(:where).with(time_period_id: 1)
+      subject.class.total_for_period(1)
+    end
+
+    it 'calculates the total costs for all expenses during a given period of time' do
+      allow(Cost).to receive(:where).and_return([cost_1, cost_3])
+      expect(Cost.total_for_period(time_period.id)).to eq 40
+    end
+  end
 end
