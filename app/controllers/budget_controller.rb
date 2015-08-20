8675750f078c7f4d3f5f7d381f3ee0970d4f1d70
Carine Vunabandi <carine.vunabandi@bt.com>
2015-08-20 10:30:25 +0100

    show list of expenses of current budget period

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -4,6 +4,7 @@ class ShekelTracker < Sinatra::Base
     @current_budget = Budget.find_by(current: true)
     @total_costs    = Cost.total_for_period(@current_budget.time_period_id)
     @status         = StatusTeller.current(@current_budget.amount, @total_costs)
+    @costs          = Cost.expenses_during(@current_budget.time_period_id)
     erb :'budget/current'
   end
 end
