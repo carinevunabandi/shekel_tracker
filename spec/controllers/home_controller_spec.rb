f5759a515b783b51d14313e8bc58ac906f6e5586
Carine Vunabandi <carine.vunabandi@bt.com>
2015-08-24 16:56:25 +0100

    Look for different headers on home page now

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,8 +1,8 @@
 describe 'GET /' do
   it 'shows the homepage heading' do
     get '/'
-    expect(last_response.body).to match(/Keeping count of your bucks/)
-    expect(last_response.body).to match(/This Month's Budget/)
-    expect(last_response.body).to match(/View Previous Budgets/)
+    expect(last_response.body).to match(/Home/)
+    expect(last_response.body).to match(/Current Budget/)
+    expect(last_response.body).to match(/Previous Budgets/)
   end
 end
