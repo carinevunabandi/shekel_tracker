efd49d0181124983cad2027ba72648f5884e57da
Carine Vunabandi <carine.vunabandi@bt.com>
2015-09-18 18:21:03 +0100

    Add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,17 @@
 ENV['RACK_ENV'] = 'test'
 
-require File.expand_path('../../shekel_tracker.rb', __FILE__)
-
 require 'byebug'
 require 'capybara/rspec'
 require 'database_cleaner'
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/spec'
+  add_filter '/vendor'
+  coverage_dir 'log/coverage/spec'
+end
+
+require File.expand_path('../../shekel_tracker.rb', __FILE__)
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods
