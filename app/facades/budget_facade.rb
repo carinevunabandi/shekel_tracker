3dfe8b5cfeada3bd28e1c71fd4f509dd3e3d25d1
Carine Vunabandi <carine.vunabandi@bt.com>
2015-10-02 23:32:53 +0100

    Add BudgetFacade and spec

--- app/facades/budget_facade.rb
+++ app/facades/budget_facade.rb
@@ -1,5 +1,41 @@
 class BudgetFacade
+  attr_reader :budget
+
   def initialize(budget)
     @budget = budget
+  end
+
+  def spending_limit
+    @budget.spending_limit
+  end
+
+  def total_spending
+    @budget.total_spending
+  end
+
+  def overspent?
+    @budget.overspent ? "Yes" : "No"
+  end
+
+  def time_period
+    "From #{@budget.from.strftime('%d-%b-%y')} to #{@budget.to.strftime('%d-%b-%y')}"
+  end
+
+  def costs
+    @costs = @budget.costs
+  end
+
+  def categories_with_totals
+    @categories = total_per_categories_for_costs
+  end
+
+  private
+
+  def total_per_categories_for_costs
+    cats_hash = {}
+    costs.map(&:category).map do |category|
+      cats_hash[category.name] = Cost.where(category: category).pluck(:price).reduce(:+)
+    end
+    cats_hash
   end
 end
---
+++ spec/facades/budget_facade_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+
+describe BudgetFacade do
+  let(:budget)        { create(:budget) }
+  let(:budget_facade) { BudgetFacade.new(budget) }
+  let(:categories)    { double(:categories) }
+  let(:costs)         { double(:costs) }
+
+  describe '#budget' do
+    it 'returns the wrapped budget object' do
+      expect(budget_facade.budget). to eq budget
+    end
+  end
+
+  describe '#spending_limit' do
+    it 'returns the spending limit set for a budget' do
+      expect(budget_facade.spending_limit).to eq 100
+    end
+  end
+
+  describe '#total_spending' do
+    it 'returns the overall total spending for a budget' do
+      expect(budget_facade.total_spending).to eq 70
+    end
+  end
+
+  describe '#overspent?' do
+    it 'returns the status of the spending rate in a budget' do
+      expect(budget_facade.overspent?).to eq "No"
+    end
+  end
+
+  describe '#time_period' do
+    it 'returns the formatted time period over which this budget applies' do
+      expect(budget_facade.time_period).to eq "01 Jan 10 => 31 Jan 10"
+    end
+
+    it "gets the 'from' date value of the current budget" do
+      expect(budget.from.strftime('%d %b %y')).to eq "01 Jan 10"
+      budget_facade.time_period
+    end
+
+    it "gets the 'to' date value of the current budget" do
+      expect(budget.to.strftime('%d %b %y')).to eq "31 Jan 10"
+      budget_facade.time_period
+    end
+  end
+
+  describe "#costs" do
+    it 'returns the costs objects associated to a budget' do
+      allow(budget).to receive(:costs).and_return(costs)
+      expect(budget_facade.costs).to eq costs
+    end
+  end
+
+  describe "#categories_with_totals" do
+    let(:categories) { (1..3).map { |number| create(:category, name:"cat_#{number}") } }
+    let(:costs) do
+      (1..3).map do |number|
+        create(:cost, category: categories[number-1], price: number*10)
+      end
+    end
+    let(:cats_and_totals)  { { "cat_1" => 10, "cat_2" => 20, "cat_3" => 30 } }
+
+    it "returns the totals per categories of costs associated to a budget" do
+      allow(budget_facade).to receive(:costs).and_return(costs)
+      expect(budget_facade.categories_with_totals).to eq cats_and_totals
+    end
+  end
+end
