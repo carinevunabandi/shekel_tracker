f86a499b585d7c6800309cbcc8d82760f4c67205
Carine Vunabandi <carine.vunabandi@bt.com>
2015-11-22 22:56:55 +0000

    add method to load all categories names

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :costs
+
+  def self.all_names
+    Category.all.map(&:name)
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe Category do
+  let!(:category_1) { create(:category, name: 'Donations') }
+  let!(:category_2) { create(:category, name: 'Transport') }
+  let!(:category_3) { create(:category, name: 'Entertainment') }
+
+  describe '.all_names' do
+    it 'gets the name of all categories from the database' do
+      expect(Category.all_names).to eq %w(Donations Transport Entertainment)
+    end
+  end
 end
